2beb6d4cca9054776db9863681e743eb94fc3bf8
Unnawut Leepaisalsuwanna <unnawut@unnawut.com>
2019-02-17 05:08:29 +0700

    Add runner-3

--- apps/load_tester/config/config.exs
+++ apps/load_tester/config/config.exs
@@ -8,7 +8,7 @@ config :load_tester,
   libcluster_topologies: [
     runners: [
       strategy: Cluster.Strategy.Epmd,
-      config: [hosts: [:"runner@runner-1", :"runner@runner-2"]],
+      config: [hosts: [:"runner@runner-1", :"runner@runner-2", :"runner@runner-3"]],
       connect: {:net_kernel, :connect_node, []},
       disconnect: {:erlang, :disconnect_node, []},
       list_nodes: {:erlang, :nodes, [:connected]}
