e5b3d4f0522fea1db24947036a86e9cc68fea961
Unnawut Leepaisalsuwanna <unnawut@unnawut.com>
2019-02-17 03:41:06 +0700

    Add missing mix local.hex --force to setup scripts

--- runner.sh
+++ runner.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mix deps.get
 mix local.rebar --force
+mix local.hex --force
 mix run -e 'LoadTester.run()'
--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mix deps.get
 mix local.rebar --force
+mix local.hex --force
 MIX_ENV=prod mix phx.server
