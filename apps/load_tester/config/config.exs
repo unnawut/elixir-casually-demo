e351d8038ff9a7847bf003548fd2125c089cb3f4
Unnawut Leepaisalsuwanna <unnawut@unnawut.com>
2019-02-19 21:24:33 +0700

    Change clustering strategy to gossip

--- apps/load_tester/config/config.exs
+++ apps/load_tester/config/config.exs
@@ -1,14 +1,16 @@
 use Mix.Config
 
+config :libcluster,
+  debug: true
+
 config :load_tester,
   num_clients: 200,
   requests_per_client: 100,
   total_duration: 10,
   target_base_url: "http://server-1:5000",
   libcluster_topologies: [
-    runners: [
-      strategy: Cluster.Strategy.Epmd,
-      config: [hosts: [:"runner@runner-1", :"runner@runner-2", :"runner@runner-3", :"runner@runner-4", :"runner@runner-5"]],
+    load_runners: [
+      strategy: Cluster.Strategy.Gossip,
       connect: {:net_kernel, :connect_node, []},
       disconnect: {:erlang, :disconnect_node, []},
       list_nodes: {:erlang, :nodes, [:connected]}
