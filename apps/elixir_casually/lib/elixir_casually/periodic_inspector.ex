133d82c96cd1b3a5367a6738e4050042f94ca385
Unnawut Leepaisalsuwanna <unnawut@unnawut.com>
2019-02-20 22:29:36 +0700

    Disable PeriodicInspector from printing to IEx

--- apps/elixir_casually/lib/elixir_casually/periodic_inspector.ex
+++ apps/elixir_casually/lib/elixir_casually/periodic_inspector.ex
@@ -21,7 +21,9 @@ defmodule ElixirCasually.PeriodicInspector do
   end
 
   def handle_info(:inspect, state) do
-    IO.puts("VoterRegistry #{inspect(Process.whereis(VoterRegistry))}, VoteCountRegistry #{inspect(Process.whereis(VoteCountRegistry))}")
+    if IEx.started?() do
+      inspect_server()
+    end
 
     schedule_inspect_self()
     {:noreply, state}
@@ -29,5 +31,9 @@ defmodule ElixirCasually.PeriodicInspector do
 
   defp schedule_inspect_self do
     Process.send_after(self(), :inspect, 5_000)
+  end
+
+  defp inspect_server do
+    IO.puts("VoterRegistry #{inspect(Process.whereis(VoterRegistry))}, VoteCountRegistry #{inspect(Process.whereis(VoteCountRegistry))}")
   end
 end
