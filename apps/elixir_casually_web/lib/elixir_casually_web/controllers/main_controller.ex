1f4cc59dd8f8620bd6ee69adb9c2e1f2e82e10db
Unnawut Leepaisalsuwanna <unnawut@unnawut.com>
2019-02-21 02:37:21 +0700

    Index minor prettify

--- apps/elixir_casually_web/lib/elixir_casually_web/controllers/main_controller.ex
+++ apps/elixir_casually_web/lib/elixir_casually_web/controllers/main_controller.ex
@@ -5,12 +5,17 @@ defmodule ElixirCasuallyWeb.MainController do
     html(conn, """
       <html>
         <head>
-          <title>ElixirCasually Voter</title>
+          <title>ElixirCasually Vote Me</title>
         </head>
         <body>
+          <h1>Vote Me</h1>
+          <h2>Be nice</h2>
           <ul>
             <li><a href="votes">Vote Summary</a></li>
             <li><a href="vote_random">Vote Randomly</a></li>
+          </ul>
+          <h2>Destroy things</h2>
+          <ul>
             <li><a href="crash/voter">Crash Voter</a></li>
             <li><a href="crash/counter">Crash Counter</a></li>
             <li><a href="crash">Crash Controller</a></li>
